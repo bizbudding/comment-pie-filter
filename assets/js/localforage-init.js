39cca435d0d996c61af0f1590fc6266c7d33526d
Mike Hemberger <mike@thestizmedia.com>
2019-06-10 17:10:35 -0400

    get it really going

--- assets/js/localforage-init.js
+++ assets/js/localforage-init.js
@@ -5,16 +5,50 @@ jQuery( function($) {
 
 	// var piedUsers  = [],
 	var $emailForm = $( '.pf-name-form' );
+	var $piedList  = $( '.pf-list' );
+	var trashCan   = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" fill="currentColor" height="2em" width="2em" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve"><g><path d="M76,26H61v-7H39v7H24v9h4.33307l3.99994,46h35.33398l3.99994-46H76V26z M41,21h18v5H41V21z M65.83301,79H34.16699   l-3.82617-44h39.31836L65.83301,79z M74,33h-2.15918H28.15918H26v-5h13h22h13V33z"/><rect x="49" y="39.94043" width="2" height="34.05957"/><polygon points="61.49805,39.94043 59.50195,39.94043 57.50195,74 59.49805,74  "/><polygon points="42.49805,74 40.49805,39.94043 38.50195,39.94043 40.50195,74  "/></g></svg>';
 
 	localforage.getItem( 'piedUsers' ).then( function( value ) {
 
-		var piedUsers = value ? value : [];
+		var piedUsers  = value ? value : [];
+		var commenters = [];
+
+		// Get the commenter names as an array.
+		var commenterNames = $( '.comment-author-name' ).map( function() {
+			return $(this).data( 'name' );
+		}).get();
+
+		// Remove already piedUsers from the array of choices.
+		commenterNames = commenterNames.filter( function( el ) {
+			return piedUsers.indexOf( el ) < 0;
+		});
+
+		// Remove duplicates.
+		commenterNames = commenterNames.filter( makeUnique );
+
+		// Add commenters object to array of commenters.
+		$.each( commenterNames, function( index, value ) {
+			commenters.push({
+				value: value,
+				label: value,
+			});
+		});
+
+		// Setup choices select field.
+		var pieChoices = new Choices( $( '[name="pf-name"]' )[0], {
+			choices: commenters,
+			searchPlaceholderValue: 'Enter commenter name...',
+			maxItemCount: 1,
+			removeItems: true,
+		});
+
+		console.log( piedUsers );
 
 		displayPiedUsers();
 		hidePiedUsers();
 
 		// Form submitted.
-		$emailForm.on( 'submit', function( e ) {
+		$emailForm.on( 'submit', function(e) {
 
 			e.preventDefault();
 
@@ -41,16 +75,38 @@ jQuery( function($) {
 			// Save the new array in storage.
 			localforage.setItem( 'piedUsers', piedUsers );
 
-			console.log( piedUsers );
+			// Clear the input.
+			pieChoices.clearInput();
+
+			// Show the list.
+			displayPiedUsers();
+		});
+
+		// Remove item if clicking a remove button.
+		$piedList.on( 'click', '.pf-remove', function(e) {
+			// Get the index of the clicked item.
+			var index = piedUsers.indexOf( $(this).data( 'value' ) );
+			// If it's a valid item.
+			if ( index !== -1 ) {
+				// Remove the item.
+				piedUsers.splice( index, 1 );
+				// Save the new array in storage.
+				localforage.setItem( 'piedUsers', piedUsers );
+				// Hide the item.
+				$(this).parent( 'li' ).fadeOut();
+			}
 		});
 
 	});
 
 	function displayPiedUsers() {
 		localforage.getItem( 'piedUsers' ).then( function( value ) {
-			// console.log( value );
-			for ( var i = 0; i < value.length; i++ ) {
-				$emailForm.append( '<div>' + value[i] + '</div>' );
+			if ( value && value.length ) {
+				$piedList.fadeOut().html( '' );
+				for ( var i = 0; i < value.length; i++ ) {
+					$piedList.append( '<li><button class="pf-remove" data-value="' + value[i] + '">' + value[i] + '&nbsp;' + trashCan + '</button></li>' );
+				}
+				$piedList.fadeIn();
 			}
 		});
 	}
@@ -73,7 +129,6 @@ jQuery( function($) {
 		$.each( $names, function( index, value ) {
 			// Get the name.
 			var $name = $(this).text();
-			// console.log( $name );
 			// Skip if not pied.
 			if ( ! $users.includes( $name ) ) {
 				return;
--- includes/class-display.php
+++ includes/class-display.php
@@ -2,16 +2,26 @@
 
 class Comment_Pie_Filter_Display {
 
+	protected $count;
+
 	function __construct() {
+		$this->count = 1;
 		$this->hooks();
 	}
 
 	function hooks() {
-		add_action( 'wp_enqueue_scripts',      array( $this, 'enqueue' ) );
-		add_action( 'genesis_before_comments', array( $this, 'add_filter' ) );
+		add_action( 'wp_enqueue_scripts',               array( $this, 'enqueue' ) );
+		add_action( 'genesis_before_comments',          array( $this, 'add_pie_filter' ) );
+		add_action( 'genesis_before_comment',           array( $this, 'comment_number' ) );
+		add_filter( 'genesis_attr_comment-author-name', array( $this, 'comment_attributes' ) );
 	}
 
 	function enqueue() {
+		wp_enqueue_style( 'comment-pie-filter', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/css/comment-pie-filter.css', array(), COMMENT_PIE_FILTER_VERSION );
+		// wp_enqueue_style( 'choices-base', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/css/choices-base.min.css', array(), '7.0.0' );
+		wp_enqueue_style( 'choices', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/css/choices.min.css', array(), '7.0.0' );
+		wp_enqueue_script( 'choices', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/js/choices.min.js', array( 'jquery' ), '7.0.0', true );
+		wp_enqueue_script( 'jquery-accessible-tabs', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/js/jquery-accessible-tabs.js', array( 'jquery' ), COMMENT_PIE_FILTER_VERSION, true );
 		wp_enqueue_script( 'localforage', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/js/localforage.js', array( 'jquery' ), COMMENT_PIE_FILTER_VERSION, true );
 		wp_enqueue_script( 'localforage-init', COMMENT_PIE_FILTER_PLUGIN_URL . 'assets/js/localforage-init.js', array( 'localforage' ), COMMENT_PIE_FILTER_VERSION, true );
 		// wp_localize_script( 'script-name', 'prefix_script_vars', array(
@@ -20,16 +30,45 @@ class Comment_Pie_Filter_Display {
 		// ) );
 	}
 
-	function add_filter() {
+	function add_pie_filter() {
 		?>
-		<div class="pie-filter">
-			<form class="pf-name-form" method="post">
-				<input type="text" name="pf-name" placeholder="Enter commenter name" value="" required>
-				<input class="pf-name-submit" type="submit" value="Submit">
-			</form>
-			<!-- TODO: Add form by comment ID, if Greg's plugin is active. -->
+		<div class="pie-filter js-tabs">
+			<ul class="js-tablist">
+				<li class="js-tablist__item">
+					<a href="#pf-form-content" class="js-tablist__link">Pie Filter</a>
+				</li>
+				<li class="js-tablist__item">
+					<a href="#pf-list-content" class="js-tablist__link">Pied List</a>
+				</li>
+			</ul>
+			<div id="pf-form-content" class="js-tabcontent">
+				<form class="pf-name-form" method="post">
+					<select name="pf-name" placeholder="Enter commenter name" value="" required>
+						<option placeholder>Select a commenter...</option>
+					</select>
+					<input class="pf-name-submit" type="submit" value="Submit">
+				</form>
+			</div>
+			<div id="pf-list-content" class="js-tabcontent">
+				<ul class="pf-list">
+				</ul>
+			</div>
 		</div>
 		<?php
+	}
+
+	function comment_number() {
+		printf( '<div class="comment-count" data-count="%1$s">%1$s.</div>', $this->count );
+		$this->count++;
+	}
+
+	function comment_attributes( $attributes ) {
+		$comment_id = get_comment_ID();
+		$comment    = get_comment( $comment_id );
+		if ( $comment ) {
+			$attributes['data-name'] = esc_html( $comment->comment_author );
+		}
+		return $attributes;
 	}
 
 }
@@ -37,9 +76,3 @@ class Comment_Pie_Filter_Display {
 add_action( 'genesis_setup', function() {
 	new Comment_Pie_Filter_Display;
 });
-
-
-// add_filter( 'get_comment', function( $_comment ) {
-	// vd( $_comment );
-	// return $_comment;
-// });
